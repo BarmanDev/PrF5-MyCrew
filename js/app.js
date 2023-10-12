003964b1f764b442a3e82168300b7790a53c4ac0
Borja <elbarmandev@gmail.com>
2023-10-12 11:37:30 +0200

    carrusel v1 finish

--- js/app.js
+++ js/app.js
@@ -1,8 +1,54 @@
 function toggleMenu() {
-    var menu = document.querySelector(".menu");
+    let menu = document.querySelector(".menu");
     if (menu.style.display === "none") {
         menu.style.display = "block";
     } else {
         menu.style.display = "none";
     }
 }
+
+const img = ["../assets/img/1.jpg", "../assets/img/2.jpg", "../assets/img/3.jpg"];
+
+let count = 0;
+
+function slider(container) {
+    container.addEventListener('click', element => {
+        let back = container.querySelector('.btn-back');
+        let forward = container.querySelector('.btn-forward'); 
+        let imgElement = container.querySelector('img');
+        let target = element.target; 
+
+        if (target === back) {
+            count = (count - 1 + img.length) % img.length; 
+            imgElement.src = img[count]; 
+
+        } else if (target === forward) {
+            count = (count + 1) % img.length;
+            imgElement.src = img[count];
+
+        }
+    });
+}
+
+function toggleSlogan() {
+    const logo = document.getElementById('logo');
+    const slogan = document.querySelector('.slogan');
+
+    logo.addEventListener('click', function (event) {
+        event.preventDefault(); // Evita que el enlace del logo redirija a otra página
+
+        if (slogan.style.display === 'none') {
+            slogan.style.display = 'block';
+        } else {
+            slogan.style.display = 'none';
+        }
+    });
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let container = document.querySelector('.container-slider');
+    slider(container);
+});
